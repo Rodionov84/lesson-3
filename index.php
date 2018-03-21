6d193fc7844c1956a49bb9f72dde83e6ab93da81
unknown <you@example.com>
2018-03-21 17:50:11 +0300

    solution

--- index.php
+++ index.php
@@ -1,120 +1,92 @@
-<!DOCTYPE html>
+<?php
+function output_array(&$array)
+{
+    foreach ( $array as $name=>$value )
+    {
+        if( is_array($value) )
+        {
+            echo "<h2>$name</h2>";
+            echo implode(", ", $value);
+        }
+        else
+        {
+            echo "<li>" . $value . "</li>";
+        }
+    }
+}
+?><!DOCTYPE html>
 <html lang="ru">
-  <head>
+<head>
     <meta charset="utf-8">
     <title>Lesson-3</title>
-  </head> 
-  <body>
+</head>
+<body>
 <?php
-
 $continents = [
     'Africa' => [
         'Gorilla beringei',
         'Loxodonta africana',
         'Bovidae antelope',
         'Hexaprotodon liberiensis'
-      ],
+    ],
     'Asia' => [
         'Cheirogaleus medius',
         'Ailuropoda melanoleuca',
         'Panthera uncia',
         'Elephas maximus'
-      ],
+    ],
     'America' => [
         'Mammuthus columbi',
         'Ophiophagus hannah',
-        'Eunectes murinus'
-      ],
+        'Eunectes murinus',
+        'Mankee'
+    ],
     'Australia' => [
         'Macropus rufus',
         'Alectura lathami',
-        'Callocephalon fimbriatum'
-      ]
-    ];
+        'Callocephalon fimbriatum',
+        'duck white big'
+    ]
+];
 
-    function output_array($array) {
-        echo "<ol>";
-        $is_num = array_values($array) == $array; // если это порядковый массив
+$new_continents = array();
+$fantasy_animals = array();
+$animals_names = array();
 
-        $keys = array_keys($array);
-        $last_elem = array_pop($keys); // извлекаем последний элемент (последний ключ)
 
-        foreach ($array as $k => $a) {
-            echo($is_num ? "<li>" : "<li>{$k}: "); // для ассоциативного массива показываем ключ элемента
-            if (is_array($a))
-                output_array($a);
-            else {
-                echo $a;
-                if ($k != $last_elem)
-                    echo ';';
-            }
-            echo "</li>";
-        }
-        echo "</ol>";
-    }
+foreach ( $continents as  $continent=>$animals)
+{
+    $new_continents[$continent] = array();
 
-    $keep_continent_name = true;
-
-    // Теперь найдите всех зверей, название которых состоит из двух слов. Составьте из них новый массив.
-    $animals2 = []; // результат
-    foreach ($continents as $cont_name => $animals) { // $animals -- array
-        foreach ($animals as $animal) { // $animal --- string
-            $words = explode(' ', $animal);
-            if (count($words) == 2) {
-                if ($keep_continent_name) {
-                    if (empty($animals2[$cont_name]))
-                        $animals2[$cont_name] = [];
-                    $animals2[$cont_name][] = $animal; // ассоциативный массив, в котором элементы - списки животных, а ключи - названия континентов
-                } else {
-                    $animals2[] = $animal; // массив строк
-                }
-            }
+    foreach ( $animals as $name )
+    {
+        $temp = explode(" ", $name);
+        if( count( $temp ) == 2 )
+        {
+            $new_continents[ $continent ][] = $name;
+            $animals_names[] = $temp[1];
         }
     }
-    if (!$keep_continent_name)
-        $animals2 = array_unique($animals2);
+}
+shuffle( $animals_names );
 
-    // вывод на экран
-    output_array($animals2);
+output_array($new_continents);
+//print_r($animals_names);
 
-    // Перемешать названия животных и составить новые
+$name_i = 0;
+foreach ( $new_continents as $continent=>$animals )
+{
+    $fantasy_animals[ $continent ] = array();
 
-    $real_animals = []; // => ['animal 1', 'animal 2', ...]
-    foreach ($continents as $animals) {
-        $real_animals = array_merge($real_animals, $animals);
+    foreach ( $animals as $name )
+    {
+        $temp = explode(" ", $name);
+        $fantasy_animals[ $continent ][] = $temp[0] . " " . $animals_names[$name_i++];
     }
-    $real_animals = array_unique($real_animals);
+}
 
-    $real_animals_words = []; /* =>
-        [0] => [первое слово названия животного 1, ... 2, ... 3, ...],
-        [1] => [второе слово названия животного 1, ... 2, ... 3, ...],
-        [...] => [...]
-    */
-    foreach ($real_animals as $animal) {
-        $words = explode(' ', $animal); // [первое, второе, ...]
-        foreach ($words as $i => $v) {
-            if (empty($real_animals_words[$i]))
-                $real_animals_words[$i] = [];
-            $real_animals_words[$i][] = $v;
-        }
-    }
-    
-    foreach ($real_animals_words as &$words) { // (&) получаем сам оригинальный объект, а не его копию
-        shuffle($words); // меняет порядок элементов этого массива произвольным способом
-    }
+output_array($fantasy_animals);
 
-    $new_animals = [];
-    while (count($real_animals_words[0]) > 0) { // while (!empty($real_animals_words[0]))
-        $w = [];
-        foreach ($real_animals_words as &$words) {
-            $w[] = array_shift($words);
-        }
-        $res = implode(' ', $w);
-        $new_animals[] = $res;
-    }
-
-    // вывод на экран
-    output_array($new_animals);
 ?>
-  </body>
-</html>   
+    </body>
+</html>
